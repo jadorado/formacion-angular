192bbe08df1d1bd04fae7d204ba62a90e720735a
natete <igonzalez@emergya.com>
2017-10-14 17:47:53 +0200

    Add groups, recordings and concerts modules to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { RecordingsModule } from './recordings/recordings.module';
+import { GroupsModule } from './groups/groups.module';
+import { ConcertsModule } from './concerts/concerts.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { LayoutModule } from './layout/layout.module';
     BrowserModule,
 
     // App modules
-    LayoutModule
+    LayoutModule,
+    ConcertsModule,
+    GroupsModule,
+    RecordingsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
