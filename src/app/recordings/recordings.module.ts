c5c9b8255ba09929d3cfe72e8dd97a72fcf677de
natete <igonzalez@emergya.com>
2017-10-14 17:46:20 +0200

    Create groups, recordings and concerts components

---
+++ src/app/recordings/recordings.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RecordingsComponent } from './recordings.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [RecordingsComponent]
+})
+export class RecordingsModule { }
