2d968db7b2834fcfe6ffd84c2897ad7019cdb02c
natete <igonzalez@emergya.com>
2017-10-14 18:25:33 +0200

    Add links in header

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
+import { RoutingModule } from '../../routing/routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RoutingModule
   ],
   declarations: [HeaderComponent],
   exports: [HeaderComponent]
