32aba9830983a10fa13567e5476c5c8641e7740a
natete <igonzalez@emergya.com>
2017-10-15 15:11:20 +0200

    Create table component

---
+++ src/app/shared/table/table.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
+})
+export class TableComponent implements OnInit {
+
+  @Input() headings: string[];
+  @Input() fields: string[];
+  @Input() data: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
