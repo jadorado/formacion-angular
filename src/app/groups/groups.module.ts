fd557b27e6b5bf9c76545c21ee687fdf53f05c46
natete <igonzalez@emergya.com>
2017-10-16 19:55:40 +0200

    Add groups section

--- src/app/groups/groups.module.ts
+++ src/app/groups/groups.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GroupsComponent } from './groups.component';
 import { GroupsRoutingModule } from './groups-routing/groups-routing.module';
+import { GroupsListComponent } from './groups-list/groups-list.component';
+import { GroupsService } from './groups.service';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    GroupsRoutingModule
+    GroupsRoutingModule,
+    SharedModule
   ],
-  declarations: [GroupsComponent]
+  declarations: [GroupsComponent, GroupsListComponent],
+  providers: [GroupsService]
 })
 export class GroupsModule { }
